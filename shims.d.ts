b894d49cacaf72bf3cda5ad51eb8e7e4445bec28
Peli de Halleux <pelikhan@users.noreply.github.com>
2018-04-17 12:19:59 -0700

    refresh tests and generated files

--- shims.d.ts
+++ shims.d.ts
@@ -1,1 +1,24 @@
-// Will be auto-generated if needed.
+// Auto-generated. Do not edit.
+
+
+    /**
+     * Mary West @ SparkFun Electronics 
+     * Ryan Mortenson https://github.com/ryanjmortenson
+     * Harry Fairhead @ IoT-Programmer 
+     * June 13, 2017
+     * https://github.com/sparkfun/pxt-weather-bit
+     *
+     * Development environment specifics:
+     * Written in Microsoft PXT
+     * Tested with a SparkFun weather:bit for micro:bit
+     *
+     * This code is released under the [MIT License](http://opensource.org/licenses/MIT).
+     * Please review the LICENSE.md file included with this example. If you have any questions 
+     * or concerns with licensing, please contact techsupport@sparkfun.com.
+     * Distributed as-is; no warranty is given.
+     */
+
+declare namespace weatherbit {
+}
+
+// Auto-generated. Do not edit. Really.
