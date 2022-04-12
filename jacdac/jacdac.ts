a7aecdcdcdf2b8ddb4c4360a484218b4912f8882
Peli de Halleux <pelikhan@users.noreply.github.com>
2022-04-11 17:48:31 -0700

    implement servers

--- jacdac/jacdac.ts
+++ jacdac/jacdac.ts
@@ -1,39 +1,5 @@
 //% deprecated
-namespace weatherbit {}
-
-namespace servers {
-    function startServers() {
-        if (!jacdac.isSimulator()) {
-            weatherbit.startRainMonitoring()
-            weatherbit.startWindMonitoring()
-            const servers: jacdac.Server[] = [
-                jacdac.createSimpleSensorServer(
-                    "rain gauge",
-                    jacdac.SRV_RAIN_GAUGE,
-                    "u16.16",
-                    () => weatherbit.rain() / 25.4, // inches -> mm
-                    {
-                        streamingInterval: 30000,
-                        readingError: () => 25.4,
-                    }
-                ),
-                jacdac.createSimpleSensorServer(
-                    "wind speed",
-                    jacdac.SRV_WIND_SPEED,
-                    "u16.16",
-                    () => weatherbit.windSpeed() * 1.60934,
-                    {
-                        streamingInterval: 2000,
-                        readingError: () => 3,
-                    }
-                ),
-            ]
-            for (const server of servers) server.start()
-        }
-        if (jacdac.checkProxy()) jacdac.proxyFinalize()
-    }
-    startServers()
-}
+namespace weatherbit { }
 
 namespace modules {
     /**
@@ -49,4 +15,35 @@ namespace modules {
      */
     //% fixedInstance whenUsed block="weatherbit wind speed"
     export const weatherbitWindSpeed = new modules.WindSpeedClient("weatherbit wind speed?device=self")
+}
+
+namespace servers {
+    function start() {
+        jacdac.startSelfServers(() => {
+            weatherbit.startRainMonitoring()
+            weatherbit.startWindMonitoring()
+            const servers: jacdac.Server[] = [
+                jacdac.createSimpleSensorServer(
+                    jacdac.SRV_RAIN_GAUGE,
+                    jacdac.RainGaugeRegPack.Precipitation,
+                    () => weatherbit.rain() / 25.4, // inches -> mm
+                    {
+                        streamingInterval: 30000,
+                        readingError: () => 25.4,
+                    }
+                ),
+                jacdac.createSimpleSensorServer(
+                    jacdac.SRV_WIND_SPEED,
+                    jacdac.WindSpeedRegPack.WindSpeed,
+                    () => weatherbit.windSpeed() * 1.60934,
+                    {
+                        streamingInterval: 2000,
+                        readingError: () => 3,
+                    }
+                ),
+            ]
+            return servers
+        })
+    }
+    start()
 }
--- jacdac/tests.ts
+++ jacdac/tests.ts
@@ -1,5 +1,5 @@
 forever(() => {
     console.logValue("rain", modules.weatherbitRain.precipitation())
-    console.logValue("wind speed", modules.weatherbitWindSpeed.windSpeed())    
-    pause(5000)
+    console.logValue("wind", modules.weatherbitWindSpeed.windSpeed())    
+    pause(1000)
 })
